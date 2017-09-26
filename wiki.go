216d4aae95e0472b107e91172e7aab2b011f53ac
Andrew Moss <1043609+amoss@users.noreply.github.com>
2017-09-26 13:41:20 +0200

    Changing paths to suit jail environment.

--- wiki.go
+++ wiki.go
@@ -58,7 +58,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
       http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("text/edit.html", "text/view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
       err := templates.ExecuteTemplate(w, tmpl+".html", p)
