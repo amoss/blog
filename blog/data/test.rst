869eebb2dd06090a594d1a969ccae60d2fc15715
Andrew Moss <1043609+amoss@users.noreply.github.com>
2017-10-09 15:06:43 +0200

    Bare bones outline of an .rst parser.
    This is basically feeling for how to write a state-machine in golang.
    Next I need the data-model that the parser should output. It would be
    roughly what the ints course used inside its build system in the
    Spring...

--- blog/data/test.rst
+++ blog/data/test.rst
@@ -18,26 +18,6 @@ A second paragraph fdk fkd kjdfk jdkfj dkf kdjkj
 jkfdjfkjdkfj kjfkdjfkdj kdjfkdjfkdjfk kdjf kjkdjf 
 fkdjfkdj kjdfkj dkkdjfk
 
-===============================================
-A title block
-===============================================
-
-:Author: the author
-:Date:   a thing
-:Tag:    some arbitrary tag
-
-Section heading
----------------------
-
-Blah blah blha aihfilsd flsdfklsdfkjsdklf jskldj
-fsdfksd 
-fsdfdsfsdf osdpf
-sdflsdkflsd
-
-A second paragraph fdk fkd kjdfk jdkfj dkf kdjkj
-jkfdjfkjdkfj kjfkdjfkdj kdjfkdjfkdjfk kdjf kjkdjf 
-fkdjfkdj kjdfkj dkkdjfk
-
 .. shell::
 
   Render as shell
--- blog/theapp.go
+++ blog/theapp.go
@@ -7,10 +7,105 @@ import (
       "strings"
 )
 
+type LineClass int
+const (
+      Blank LineClass = iota
+      Indented
+      SectionHeading
+      SubsectionHeading
+      Directive
+      Other
+)
+
+// Simple abstract domain for the types of lines in an .rst
+func classifyLine(line string) LineClass {
+  if len(line)==0 {
+    return Blank
+  }
+  // If you are not me then don't use tabs, haha only serious
+  expanded := strings.Replace(line,"\t","  ",-1)
+  text := strings.TrimLeft( expanded," " )
+  indent := len(expanded) - len(text)
+  if indent>0 {
+    return Indented
+  }
+
+  if len(strings.Trim(expanded,"="))==0 {
+    return SectionHeading
+  }
+  if len(strings.Trim(expanded,"-"))==0 {
+    return SubsectionHeading
+  }
+  if line[0:2]==".." {
+    return Directive
+  }
+  return Other
+}
+
+type ParseState int
+const (
+        Default ParseState = iota
+        TitleBlock
+        InPara
+        InDirective
+)
+
 func parseRst(src string) {
   lines := strings.Split(src,"\n")
+  state := Default
+  cur := ""
   for i:= 0; i<len(lines); i++ {
-    fmt.Println(i, lines[i])
+    switch state {
+      case Default:
+        switch classifyLine(lines[i]) {
+          case SectionHeading:
+            state = TitleBlock
+          case Blank:
+            // Ignore
+          case Other:
+            state = InPara
+            cur   = lines[i]
+          case Directive:
+            state = InDirective
+            cur = lines[i][3:]
+        }
+      case InDirective:
+        switch classifyLine(lines[i]) {
+          case Indented:
+            cur += "\n" + lines[i]    // Do a tab-expansion and TrimLeft
+          case Blank:
+            fmt.Println("Found a directive", cur)
+            state = Default
+            cur   = ""
+        }
+
+      case TitleBlock:
+        switch classifyLine(lines[i]) {
+          case Other:
+            cur += "\n" + lines[i]
+          case SectionHeading:
+            fmt.Println("Found a title block", cur)
+            state = Default
+        }
+      case InPara:
+        switch classifyLine(lines[i]) {
+          case Other:
+            cur += "\n" + lines[i]
+          case Blank:
+            fmt.Println("Found a para", cur)
+            state = Default
+            cur   = ""
+          case SectionHeading:
+            fmt.Println("Found a section heading", cur)
+            cur = ""
+            state = Default
+          case SubsectionHeading:
+            fmt.Println("Found a subsection heading", cur)
+            cur = ""
+            state = Default
+        }
+    }
+    fmt.Println(i, classifyLine(lines[i]), lines[i])
   }
 }
 
