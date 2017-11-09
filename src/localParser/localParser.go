044b5266949f614faa2687402563997291e26f97
Andrew Moss <1043609+amoss@users.noreply.github.com>
2017-11-09 14:39:15 +0100

    Separate binary for local dev work.

---
+++ src/localParser/localParser.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "os"
+  "rst"
+  "fmt"
+)
+
+func main() {
+    output := false
+    for _,arg := range os.Args[2:] {
+        switch arg {
+            case "--lines":  rst.LineScannerDbg  = true
+            case "--parse":  rst.LineParserStDbg = true
+            case "--blocks": rst.LineParserDbg   = true
+            case "--html":   output = true
+            default: panic("Unrecognised arg "+arg)
+        }
+    }
+    lines  := rst.LineScanner(os.Args[1])
+    if lines!=nil {
+        blocks := rst.Parse(*lines)
+        res := rst.RenderHtml(blocks)
+        if output { fmt.Println(string(res)) }
+    }
+}
