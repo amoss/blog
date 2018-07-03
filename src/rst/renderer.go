c217c862ca65da972c41c02ae2121cfc00041963
Andrew Moss <1043609+amoss@users.noreply.github.com>
2018-07-03 16:32:24 +0200

    Fixing up style.

--- src/rst/renderer.go
+++ src/rst/renderer.go
@@ -177,7 +177,7 @@ func renderHtmlPage(headBlock Block, input chan Block) []byte {
                 result = append(result, []byte(html.EscapeString(string(blk.Body)))... )
                 result = append(result, []byte("</pre>")... )
             case BlkCode:
-                result = append(result, []byte("<pre class=\"code\"><table style=\"border-collapse: collapse\">")... )
+                result = append(result, []byte("<pre class=\"code\"><table style=\"width: 100%;border-collapse: collapse\">")... )
                 fmt.Print(string(blk.Body))
                 for _,line := range strings.Split(string(blk.Body),"\n") {
                     result = append(result, []byte("<tr><td class=\"lnum\"></td><td class=\"content\"><code>")... )
