3a3f3ae172298c5fa0680e6e95fdc42809513b79
Andrew Moss <1043609+amoss@users.noreply.github.com>
2018-07-04 10:08:05 +0200

    Fixing up style.

--- src/rst/renderer.go
+++ src/rst/renderer.go
@@ -182,7 +182,7 @@ func renderHtmlPage(headBlock Block, input chan Block) []byte {
                 for _,line := range strings.Split(string(blk.Body),"\n") {
                     result = append(result, []byte("<tr><td class=\"lnum\"></td><td class=\"content\"><code>")... )
                     result = append(result, []byte(html.EscapeString(line))... )
-                    result = append(result, byte('\n'))
+                    //result = append(result, byte('\n'))
                     result = append(result, []byte("</code></td></tr>")... )
                 }
                 result = append(result, []byte("</table></pre>")... )
